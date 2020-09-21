f6050a22d4a0a82615be71fdb8e720deeca59db4
Johan Thoren <jthoren@itrsgroup.com>
2020-09-21 08:34:42 +0200

    Added eth to wired script.

--- wired.sh
+++ wired.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 INET="$(ip addr | grep 'inet\ ')"
 ENP="$(grep enp <<< "$INET")"
+ETH="$(grep eth <<< "$INET")"
 
 ENP_IP_CUT="${ENP#*inet\ }"
+ETH_IP_CUT="${ETH#*inet\ }"
 ENP_IP="${ENP_IP_CUT%%\/*}"
+ETH_IP="${ETH_IP_CUT%%\/*}"
 
 MSG="❎"
 #[ -n "$ENP_IP" ] && MSG="\x01🖧\x01"
 
 # Only show the status if there is something interesting to show.
-[ -n "$ENP_IP" ] && exit 0
+if [ -n "$ENP_IP" ] || [ -n "$ETH_IP" ]; then
+    exit 0
+fi
 
 echo -e " $MSG "
