5d30b6f8efc4de30fcd6b0c4d96599f67151752e
Johan Thoren <johan@thoren.xyz>
2020-10-03 21:54:43 +0200

    More compact mail script.

--- mail.sh
+++ mail.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 UNREAD="$(mu find flag:unread AND NOT flag:trashed AND NOT maildir:/Junk 2>/dev/null | wc -l)"
 
-if [ "$UNREAD" -eq 0 ]; then
-    exit 0
-fi
-
-MSG="✉ $UNREAD"
-
-echo " $MSG "
+[ "$UNREAD" -eq 0 ] || echo " 📧 "
