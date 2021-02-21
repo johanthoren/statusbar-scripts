61641f69a22381688a70074710056b0bab024153
Johan Thoren <johan@thoren.xyz>
2021-02-21 22:45:31 +0100

    Add docker partition.

--- free_disk.sh
+++ free_disk.sh
@@ -12,18 +12,23 @@ get_free() {
 
 ROOT_STATUS="$(get_free "/")"
 HOME_STATUS="$(get_free "/home")"
+DOCKER_STATUS="$(get_free "/var/lib/docker")"
 
 # Only show information if there is something interesting to show.
 ROOT_FREE=""
 HOME_FREE=""
+DOCKER_FREE=""
 if [ ! "$ROOT_STATUS" = 1 ]; then
     ROOT_FREE="$ROOT_STATUS"
 fi
 if [ ! "$HOME_STATUS" = 1 ]; then
     HOME_FREE="$HOME_STATUS"
 fi
+if [ ! "$DOCKER_STATUS" = 1 ]; then
+    DOCKER_FREE="$DOCKER_STATUS"
+fi
 
-MSG="${ROOT_FREE}${HOME_FREE}"
+MSG="${ROOT_FREE}${HOME_FREE}${DOCKER_FREE}"
 
 [ -z "$MSG" ] && exit 0
 echo -e " 💽 ${MSG} "
