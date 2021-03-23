ea6098dd9482e65f9b99ffea8d012ed6ef5334fe
Johan Thoren <johan@thoren.xyz>
2021-03-23 12:08:00 +0100

    Add support for OpenBSD.

--- free_disk.sh
+++ free_disk.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+OS=$(uname)
+
 MSG=""
+
 get_free() {
     df_out="$(df -h | grep "$1"\$)"
     free="$(awk '{print $4}' <<< "$df_out")"
@@ -7,28 +10,81 @@ get_free() {
     perc="${perc%\%}"
     [ "$perc" -le 89 ] && return 1
 
-    echo "${1} $free"
+    echo "${1} $free "
 }
 
-ROOT_STATUS="$(get_free "/")"
-HOME_STATUS="$(get_free "/home")"
-DOCKER_STATUS="$(get_free "/var/lib/docker")"
+if [ "$OS" = "Linux" ]; then
+	ROOT_STATUS="$(get_free "/")"
+	HOME_STATUS="$(get_free "/home")"
+	DOCKER_STATUS="$(get_free "/var/lib/docker")"
 
-# Only show information if there is something interesting to show.
-ROOT_FREE=""
-HOME_FREE=""
-DOCKER_FREE=""
-if [ ! "$ROOT_STATUS" = 1 ]; then
-    ROOT_FREE="$ROOT_STATUS"
-fi
-if [ ! "$HOME_STATUS" = 1 ]; then
-    HOME_FREE="$HOME_STATUS"
-fi
-if [ ! "$DOCKER_STATUS" = 1 ]; then
-    DOCKER_FREE="$DOCKER_STATUS"
-fi
+	# Only show information if there is something interesting to show.
+	ROOT_FREE=""
+	HOME_FREE=""
+	DOCKER_FREE=""
+	if [ ! "$ROOT_STATUS" = 1 ]; then
+    		ROOT_FREE="$ROOT_STATUS"
+	fi
+	if [ ! "$HOME_STATUS" = 1 ]; then
+    		HOME_FREE="$HOME_STATUS"
+	fi
+	if [ ! "$DOCKER_STATUS" = 1 ]; then
+    		DOCKER_FREE="$DOCKER_STATUS"
+	fi
 
-MSG="${ROOT_FREE}${HOME_FREE}${DOCKER_FREE}"
+	MSG="${ROOT_FREE}${HOME_FREE}${DOCKER_FREE}"
+
+elif [ "$OS" = "OpenBSD" ]; then
+	ROOT_STATUS="$(get_free "/")"
+	HOME_STATUS="$(get_free "/home")"
+	TMP_STATUS="$(get_free "/tmp")"
+	USR_STATUS="$(get_free "/usr")"
+	X11R6_STATUS="$(get_free "/usr/X11R6")"
+	LOCAL_STATUS="$(get_free "/usr/local")"
+	OBJ_STATUS="$(get_free "/usr/obj")"
+	SRC_STATUS="$(get_free "/usr/src")"
+	VAR_STATUS="$(get_free "/var")"
+
+	# Only show information if there is something interesting to show.
+	ROOT_FREE=""
+	HOME_FREE=""
+	TMP_FREE=""
+	USR_FREE=""
+	X11R6_FREE=""
+	LOCAL_FREE=""
+	OBJ_FREE=""
+	SRC_FREE=""
+	VAR_FREE=""
+	if [ ! "$ROOT_STATUS" = 1 ]; then
+    		ROOT_FREE="$ROOT_STATUS"
+	fi
+	if [ ! "$HOME_STATUS" = 1 ]; then
+    		HOME_FREE="$HOME_STATUS"
+	fi
+	if [ ! "$TMP_STATUS" = 1 ]; then
+    		TMP_FREE="$TMP_STATUS"
+	fi
+	if [ ! "$USR_STATUS" = 1 ]; then
+    		USR_FREE="$USR_STATUS"
+	fi
+	if [ ! "$X11R6_STATUS" = 1 ]; then
+    		X11R6_FREE="$X11R6_STATUS"
+	fi
+	if [ ! "$LOCAL_STATUS" = 1 ]; then
+    		LOCAL_FREE="$LOCAL_STATUS"
+	fi
+	if [ ! "$OBJ_STATUS" = 1 ]; then
+    		OBJ_FREE="$OBJ_STATUS"
+	fi
+	if [ ! "$SRC_STATUS" = 1 ]; then
+    		SRC_FREE="$SRC_STATUS"
+	fi
+	if [ ! "$VAR_STATUS" = 1 ]; then
+    		VAR_FREE="$VAR_STATUS"
+	fi
+
+	MSG="${ROOT_FREE}${HOME_FREE}${TMP_FREE}${USR_FREE}${X11R6_FREE}${LOCAL_FREE}${OBJ_FREE}${SRC_FREE}${VAR_FREE}"
+fi
 
 [ -z "$MSG" ] && exit 0
-echo -e " 💽 ${MSG} "
+echo -e " ð½ ${MSG} "
