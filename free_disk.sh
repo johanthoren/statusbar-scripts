355567c1951c7840bb0a3cf707f8def5b5623b5f
Johan Thoren <johan@thoren.xyz>
2021-04-07 16:44:59 +0200

    Remove docker partition.

--- free_disk.sh
+++ free_disk.sh
@@ -8,7 +8,7 @@ get_free() {
     free="$(awk '{print $4}' <<< "$df_out")"
     perc="$(awk '{print $5}' <<< "$df_out")"
     perc="${perc%\%}"
-    [ "$perc" -le 89 ] && return 1
+    [ "$perc" -le 94 ] && return 1
 
     echo "${1} $free "
 }
@@ -16,23 +16,18 @@ get_free() {
 if [ "$OS" = "Linux" ]; then
 	ROOT_STATUS="$(get_free "/")"
 	HOME_STATUS="$(get_free "/home")"
-	DOCKER_STATUS="$(get_free "/var/lib/docker")"
 
 	# Only show information if there is something interesting to show.
 	ROOT_FREE=""
 	HOME_FREE=""
-	DOCKER_FREE=""
 	if [ ! "$ROOT_STATUS" = 1 ]; then
     		ROOT_FREE="$ROOT_STATUS"
 	fi
 	if [ ! "$HOME_STATUS" = 1 ]; then
     		HOME_FREE="$HOME_STATUS"
 	fi
-	if [ ! "$DOCKER_STATUS" = 1 ]; then
-    		DOCKER_FREE="$DOCKER_STATUS"
-	fi
 
-	MSG="${ROOT_FREE}${HOME_FREE}${DOCKER_FREE}"
+	MSG="${ROOT_FREE}${HOME_FREE}"
 
 elif [ "$OS" = "OpenBSD" ]; then
 	ROOT_STATUS="$(get_free "/")"
