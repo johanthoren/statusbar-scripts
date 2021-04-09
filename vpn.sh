8ff880dcb4fafc1c1ddff3f8327d798d3b7fb33c
Johan Thoren <johan@thoren.xyz>
2021-04-09 14:30:37 +0200

    Replace vpn.sh with mullvad.sh and rename.

--- vpn.sh
+++ vpn.sh
@@ -1,39 +1,51 @@
 #!/usr/bin/env bash
+set -euo pipefail
+# Make sure to have $WORK_IP and $WORK_NAME in your $HOME/.profile
+# if you want to use the IP match exception. Else, remove this part.
+source "$HOME/.profile"
+
 MSG=""
 
-if command -v mullvad &> /dev/null; then
-    relay_info=$(mullvad relay get)
-    relay_info="${relay_info##*city\ }"
-    country=$(awk '{ print toupper ($2) }' <<< $relay_info)
-    city_code=$(awk '{ print $1 }' <<< $relay_info)
-    city_code=$(sed 's/,//' <<< $city_code)
-    relay_list_file="/tmp/mullvad_relay_list.txt"
-    [ -s "$relay_list_file" ] || mullvad relay list > "$relay_list_file"
-    relay_list=$(<"$relay_list_file")
-    city_name=$(grep \(${city_code}\) <<< $relay_list | head -n1 | awk -F '(' '{ print $1 }' | awk -F , '{ print $1 }' | sed 's/^\s*//' | sed 's/\s*$//')
-    vpn_status=$(mullvad status | awk '{print $3}')
+IFS=$'\n'
+response=($(curl -s https://am.i.mullvad.net/json | \
+            jq '.["ip", "country", "mullvad_exit_ip"]'))
 
-    case "$vpn_status" in
-        Connected)
-            MSG="🔒 ${city_name}, ${country}"
-            ;;
-        Connecting)
-            MSG="🔓 ${vpn_status}"
-            ;;
-        Disconnected)
-            MSG="🔓 ${vpn_status}"
-            ;;
-        *)
-            MSG="🔓 Unknown"
-    esac
-elif [ "$(hostname)" = "t490" ]; then
-    vpn_if="ppp0"
-    inet="$(ip addr | grep 'inet\ ')"
-    vpn="$(grep "$vpn_if" <<< "$inet")"
-    vpn_ip_cut="${vpn#*inet\ }"
-    vpn_ip="${vpn_ip_cut%% peer*}"
-    [ -n "$vpn_ip" ] && MSG="🌐 ${vpn_ip}"
-fi
+[ "${#response[@]}" -ne 3 ] && echo "🔓 Unknown" && exit 1
+
+ip_str="${response[0]}"
+country_str="${response[1]}"
+mullvad_exit_ip="${response[2]}"
+
+ip_tmp="${ip_str%\"}"
+ip="${ip_tmp#\"}"
+
+country_tmp="${country_str%\"}"
+country="${country_tmp#\"}"
+
+case "$mullvad_exit_ip" in
+    true)
+        MSG="🔒 ${country}"
+        ;;
+    false)
+        # Uncomment the line below and remove the whole case statement to
+        # skip this very personal check.
+        #MSG="🔓 ${ip}, ${country}"
+
+        # Remove from here:
+        case "${ip}" in
+            # My workpace VPN IP and a msg indicating connection.
+            "${WORK_IP}")
+                MSG="🔒 ${WORK_NAME}"
+                ;;
+            *)
+                MSG="🔓 ${ip}, ${country}"
+                ;;
+        esac
+        # Remove until here to disable the above mentioned check
+        ;;
+    *)
+        MSG="🔓 Unknown"
+esac
 
 [ -n "$MSG" ] || exit 1
 echo " ${MSG} "
