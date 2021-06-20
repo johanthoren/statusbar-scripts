bba2197a357561a61992a11a2594e0c7ee13b602
Johan Thoren <johan@thoren.xyz>
2021-06-20 10:59:40 +0200

    Don't show anything if at home.

--- home_or_work.sh
+++ home_or_work.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 MSG=""
 
 [ -f "$HOME/.at_work" ] && MSG="💼 At work"
-[ -f "$HOME/.at_home" ] && MSG="🏡 At home"
+#[ -f "$HOME/.at_home" ] && MSG="🏡 At home"
 
 [ -n "$MSG" ] || exit 1
 echo " ${MSG} "
