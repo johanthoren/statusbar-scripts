ce2f9a3ccfffda6fac9228c77bcc662302f0fcf7
Johan Thoren <johan@thoren.xyz>
2021-08-28 11:50:48 +0200

    Break out feasts to its own script.

---
+++ feast.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+if ! command -v bibcal &> /dev/null; then
+    echo "command not found: bibcal"
+    exit 127
+fi
+
+day=$(bibcal -t)
+
+major_feast="$(awk '/Major/ {print $6}' <<< "$day")"
+minor_feast="$(awk '/Minor/ {print $6}' <<< "$day")"
+
+if [ ! "$major_feast" = "false" ]; then
+    echo " $major_feast "
+    exit 0
+elif [ ! "$minor_feast" = "false" ]; then
+    echo " $minor_feast "
+    exit 0
+else
+    exit 1
+fi
--- sabbath.sh
+++ sabbath.sh
@@ -4,21 +4,5 @@ if ! command -v bibcal &> /dev/null; then
     exit 127
 fi
 
-day=$(bibcal -t)
-
-major_feast="$(awk '/Major/ {print $6}' <<< "$day")"
-minor_feast="$(awk '/Minor/ {print $6}' <<< "$day")"
-sabbath="$(awk '/Sabbath/ {print $4}' <<< "$day")"
-
-if [ ! "$major_feast" = "false" ]; then
-    echo " $major_feast "
-    exit 0
-elif [ ! "$minor_feast" = "false" ]; then
-    echo " $minor_feast "
-    exit 0
-elif [ ! "$sabbath" = "false" ]; then
-    echo " Sabbath "
-    exit 0
-else
-    exit 1
-fi
+bibcal -s && echo " Shabbat " && exit 0
+exit 1
