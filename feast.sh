431ad2b1b6382b5db849ffe4e1e635d7412f2f6b
Johan Thoren <johan@thoren.xyz>
2021-09-08 22:07:44 +0200

    Fix issue with calculating feast.

--- feast.sh
+++ feast.sh
@@ -4,18 +4,14 @@ if ! command -v bibcal &> /dev/null; then
     exit 127
 fi
 
-day=$(bibcal -t)
-major="$(grep 'Major' <<< "$day")"
-major="$(grep 'Minor' <<< "$day")"
+major="$(bibcal -t | grep 'Major' | cut -d ' ' -f4- | sed 's/^\s*//g')"
+minor="$(bibcal -t | grep 'Minor' | cut -d ' ' -f4- | sed 's/^\s*//g')"
 
-major_feast="$(cut -d ' ' -f4- <<< "$major" | sed 's/^\s*//g')"
-minor_feast="$(cut -d ' ' -f4- <<< "$minor" | sed 's/^\s*//g')"
-
-if [ ! "$major_feast" = "false" ]; then
-    echo " $major_feast "
+if [ ! "$major" = "false" ]; then
+    echo " $major "
     exit 0
-elif [ ! "$minor_feast" = "false" ]; then
-    echo " $minor_feast "
+elif [ ! "$minor" = "false" ]; then
+    echo " $minor "
     exit 0
 else
     exit 1
